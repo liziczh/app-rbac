b4f719aac903ad060b85c1bf7a455770c711cbef
liziczh <liziczh@foxmail.com>
2020-05-05 00:53:28 +0800

    init dir

--- rbac-api/src/main/java/com/liziczh/rbac/api/ApiApplication.java
+++ rbac-api/src/main/java/com/liziczh/rbac/api/ApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RbacApiApplication {
+public class ApiApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RbacApiApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 }
